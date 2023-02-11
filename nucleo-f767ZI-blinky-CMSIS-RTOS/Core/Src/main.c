6d37911d1715367013e0b42c9eb7f99e76a09164
Benedict Hofmockel <dubmh324@gmail.com>
2023-02-10 21:03:42 -0500

    Enabled ch3 and ch10 on ADC1

--- nucleo-f767ZI-blinky-CMSIS-RTOS/Core/Src/main.c
+++ nucleo-f767ZI-blinky-CMSIS-RTOS/Core/Src/main.c
@@ -22,7 +22,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <string.h>
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -237,13 +238,13 @@ static void MX_ADC1_Init(void)
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.NbrOfConversion = 2;
   hadc1.Init.DMAContinuousRequests = DISABLE;
   hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
@@ -256,6 +257,15 @@ static void MX_ADC1_Init(void)
   sConfig.Channel = ADC_CHANNEL_3;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_10;
+  sConfig.Rank = ADC_REGULAR_RANK_2;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -400,12 +410,13 @@ static void MX_GPIO_Init(void)
 /* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void *argument)
 {
+  /* USER CODE BEGIN 5 */
 	uint16_t raw;
 	char msg[10];
-  /* USER CODE BEGIN 5 */
   /* Infinite loop */
   for(;;)
   {
+	  //#############################APPS 1#####################################
 	  //What one can do to test the Timing of the ADC is pull a pin high here and measure it wit the Oscilloscope
 	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
 
@@ -418,11 +429,27 @@ void StartDefaultTask(void *argument)
 	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
 
 	  //Convert string and send message over UART to PC console
-	  sprintf(msg,"XD%hu\r\n", raw);
+	  sprintf(msg,"%hu\r\n", raw);
 	  HAL_UART_Transmit(&huart3, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
 
-	  //Pretend we have something else to do for a while
-	  HAL_Delay(1);
+	  //#############################APPS 2#####################################
+//	  //What one can do to test the Timing of the ADC is pull a pin high here and measure it wit the Oscilloscope
+//	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
+//
+//	  //Get ADC Value
+//	  HAL_ADC_Start(&hadc1);
+//	  HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+//	  raw = HAL_ADC_GetValue(&hadc1);
+//
+//	  //Set GPIO pin low for ADC timing on Oscilloscope
+//	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
+//
+//	  //Convert string and send message over UART to PC console
+//	  sprintf(msg,"A1: %hu\r\n", raw);
+//	  HAL_UART_Transmit(&huart3, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+//
+//	  //Pretend we have something else to do for a while
+//	  HAL_Delay(1);
   }
 
   //In case the thread is ever terminated (exited from task loop), which should be never
