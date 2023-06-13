638b34359794c526695bcf23cd365ce6b13d2baf
ColeBardin <cab572@drexel.edu>
2023-06-13 17:28:34 -0400

    refactored apps_task. also sets rfe low on apps error

--- nucleo-f767ZI-drev23-ecu-firmware/Core/Src/tasks/apps_task.c
+++ nucleo-f767ZI-drev23-ecu-firmware/Core/Src/tasks/apps_task.c
@@ -9,6 +9,7 @@
  *
  */
 
+#include "main.h"
 #include "tasks/apps_task.h"
 #include "ext_drivers/canbus.h"
 
@@ -57,26 +58,28 @@ void apps_task_fn(void *arg) {
         // Record number of ticks at entry of each loop 
         entryTicksCount = osKernelGetTickCount();
 
-        // Read APPS potentiometers
-        adc_raw[0] = apps1->read_count((void *)apps1);
-        adc_raw[1] = apps2->read_count((void *)apps2);
-
-        // Convert to a floating point percentage
-        throttle_percent[0] = adc_raw_to_percent(apps1, adc_raw[0]);
-        throttle_percent[1] = adc_raw_to_percent(apps2, adc_raw[1]);
-
-        if(!check_implausability(throttle_percent[0], throttle_percent[1])){
-            // If plausibility check fails, set flag until soft reset
-        	data->rtd_flag = true;
-        }
-
-        // Average throttle percents
-        avg_throttle = (throttle_percent[0] + throttle_percent[1]) / 2;
-        data->torque = avg_throttle;
-        // Convert to hex number for Bamocar register value
-        torque_hex = percent_to_trq_hex(avg_throttle);
-
         if(data->rtd_flag == false){
+            // Read APPS potentiometers
+            adc_raw[0] = apps1->read_count((void *)apps1);
+            adc_raw[1] = apps2->read_count((void *)apps2);
+
+            // Convert to a floating point percentage
+            throttle_percent[0] = adc_raw_to_percent(apps1, adc_raw[0]);
+            throttle_percent[1] = adc_raw_to_percent(apps2, adc_raw[1]);
+
+            if(!check_implausability(throttle_percent[0], throttle_percent[1])){
+                // If plausibility check fails, set flag until soft reset
+                data->rtd_flag = true;
+                // Set RFE low, disable motor
+                HAL_GPIO_WritePin(BAMOCAR_RFE_Activate_GPIO_Port, BAMOCAR_RFE_Activate_Pin, 0);
+            }
+
+            // Average throttle percents
+            avg_throttle = (throttle_percent[0] + throttle_percent[1]) / 2;
+            data->torque = avg_throttle;
+            // Convert to hex number for Bamocar register value
+            torque_hex = percent_to_trq_hex(avg_throttle);
+
             // When flag is not set, send normal torque command
             tx_packet.data[1] = TRQ_HEX_TO_LSB(torque_hex);
             tx_packet.data[2] = TRQ_HEX_TO_MSB(torque_hex);
